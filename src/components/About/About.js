4f2857dddf8269187ca7827b3c381c2bad60870f
sannypro <sannymr79@gmail.com>
2022-04-18 15:12:32 +0600

    all done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ import "./About.css"
 const About = () => {
     return (
         <div className='d-flex mt-5 about align-items-center'>
-            <div className='w-50  mx-auto  shadow'>
+            <div className=' mx-auto about-details shadow'>
                 <h1 className='text-center mt-2' >About me</h1>
                 <p className='p-5'>This is Mahibur Rahman Sanny. Working Hard to achieve my goal. And My Goal is to be a MERN stack developer. I  Practice 8 hrs per day to achieve my goal </p>
             </div>
